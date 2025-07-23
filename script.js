eadf58b6e370f5b13b0934b9e08d07663ef518ee
Beba2508 <info@irundo.com>
2025-07-23 19:28:13 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const today = new Date();
+  const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+  const formattedDate = today.toLocaleDateString("hr-HR", options);
+  document.getElementById("date").textContent = formattedDate;
+});
