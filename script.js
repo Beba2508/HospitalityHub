fef0dc3900985ff36075c9e4baba3fa2ee075ddb
Beba2508 <info@irundo.com>
2025-07-23 19:43:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const today = new Date();
-  const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  };
+
   const formattedDate = today.toLocaleDateString("hr-HR", options);
   document.getElementById("date").textContent = formattedDate;
 });
